378f9464205cef6ec116c618b04abc098fd046ab
nakadeuttam <nakadeuttam66@gmail.com>
2023-11-29 18:18:13 +0530

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div>
-      <p style={{textAlign: 'right', marginRight :'50px', marginTop:'220px',}}>&copy;Copyright-UGN.pvt.ltd </p>
-    </div>
-  )
-}
